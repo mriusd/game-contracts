57f7f64a8d9360cda039c08b3191ea4d75a6af6c
mariusd <mariusdot@protonmail.com>
2023-03-30 17:43:53 +0300

    set box drop func

--- items.sol
+++ items.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -11,7 +11,10 @@ contract Items is ERC721 {
 
     mapping (uint256 => string) public itemName;
 
+    
+
     struct ItemAttributes {
+        uint tokenId;
         uint itemLevel;
         uint maxLevel;
         uint durability;
@@ -79,8 +82,18 @@ contract Items is ERC721 {
         bool isBox;
     }
 
+    mapping (uint256 => ItemAttributes) private _tokenAttributes;
+    mapping (uint256 => ItemAttributes) private _itemAttributes;
+    Counters.Counter private _tokenIdCounter;
+
+    ItemAttributes boxAttributes;
+
     constructor() ERC721("Combats", "Item") {
         owner = msg.sender;
+
+        boxAttributes.isBox = true;
+        boxAttributes.itemWidth = 1;
+        boxAttributes.itemHeight = 1;
     }
 
     struct DropList {
@@ -95,9 +108,9 @@ contract Items is ERC721 {
         uint256[] miscList;
     }
 
-    mapping (uint256 => DropList) public boxDropList;
+    mapping (uint => DropList) public boxDropList;
 
-    event BoxDropUpdated(uint256 level, uint256 weaponsDropRate, uint256 armourDropRate, uint256 jewelsDropRate, uint256 miscDropRate);
+    event BoxDropUpdated(uint level, uint256 weaponsDropRate, uint256 armourDropRate, uint256 jewelsDropRate, uint256 miscDropRate);
 
     function setBoxDrop (
         uint256 level, 
@@ -122,6 +135,124 @@ contract Items is ERC721 {
         emit BoxDropUpdated(level, dropRates[0], dropRates[1], dropRates[2], dropRates[3]);
     }
 
-    
+    event BoxDropped(address owner, uint256 tokenId);
+    function dropBox (uint256 tokenId) external returns (uint256) {
+
+        require(_exists(tokenId), "Token does not exist");
+        require(_tokenAttributes[tokenId].isBox, "Not a box");
+        uint256 boxLevel = _tokenAttributes[tokenId].itemLevel;
+        
+        DropList memory dropList = boxDropList[boxLevel];
+
+        uint256 totalProbability = safeAdd(dropList.weaponsDropRate, safeAdd(dropList.armourDropRate, safeAdd(dropList.jewelsDropRate, dropList.miscDropRate)));
+
+        uint256 randomNum = getRandomNumber();
+
+        uint256 droppedItemId = 0;
+
+        bool isWeapon;
+        bool isArmour; 
+        bool isJewel; 
+        bool isMisc;
+
+        if (randomNum <= totalProbability) {
+            if (randomNum <= dropList.weaponsDropRate)    {
+                isWeapon = true;
+                droppedItemId = returnRandomItemFromDropList(dropList.weaponsList);
+            } else if (randomNum <= safeAdd(dropList.weaponsDropRate, dropList.armourDropRate)) {
+                isArmour = true;
+                droppedItemId = returnRandomItemFromDropList(dropList.armourList);
+            } else if (randomNum <= safeAdd(dropList.jewelsDropRate, safeAdd(dropList.weaponsDropRate, dropList.armourDropRate))) {
+                isJewel = true;
+                droppedItemId = returnRandomItemFromDropList(dropList.jewelsList);
+            } else {
+                isMisc = true;
+                droppedItemId = returnRandomItemFromDropList(dropList.miscList);
+            }
+        } else {
+            return 0;
+        }
+
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(msg.sender, tokenId);
+        _setTokenAttributes(tokenId, _itemAttributes[droppedItemId]);
+
+        // Determine Excelent option
+        //uint excelentProbability = boxDropList[_tokenAttributes[tokenId].itemLevel];
+
+        _tokenAttributes[tokenId].tokenId = tokenId;       
+
+        emit BoxDropped(msg.sender, tokenId);
+
+        return tokenId;
+    }
+
+    function mintBox(address winner, uint256 opponentLevel) external {
+        uint256 boxLevel = opponentLevel/50;
+
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(winner, tokenId);
+
+        ItemAttributes memory newBoxAttributes = boxAttributes;
+
+        boxAttributes.itemLevel = boxLevel; 
+
+        _setTokenAttributes(tokenId, boxAttributes);
+    }
+
+    function _setTokenAttributes(uint256 tokenId, ItemAttributes memory attrs) internal {
+        require(_exists(tokenId), "Token does not exist");
+
+        _tokenAttributes[tokenId] = attrs;
+    }
+
+    function returnRandomItemFromDropList(uint256[] memory items) internal returns (uint256) {
+        uint256 randomNumber = getRandomNumber();
+        uint256 len = items.length;
+
+        return items[randomNumber % len];
+
+    }
+
+    function getRandomNumber () internal returns (uint256) {
+        uint256 randomNumber = block.prevrandao;
+
+        uint256 modulus = randomNumber % 100;
+
+        return randomNumber;
+    }
+
+
+    // Returns the smaller of two values
+    function min(uint a, uint b) private pure returns (uint) {
+        return a < b ? a : b;
+    }
+
+    // Returns the largest of the two values
+    function max(uint a, uint b) private pure returns (uint) {
+        return a > b ? a : b;
+    }
+
+    // Safe Multiply Function - prevents integer overflow 
+    function safeMul(uint a, uint b) public returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    // Safe Subtraction Function - prevents integer overflow 
+    function safeSub(uint a, uint b) public returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    // Safe Addition Function - prevents integer overflow 
+    function safeAdd(uint a, uint b) public returns (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
+        return c;
+    }
 
 }
